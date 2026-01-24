1b0daa59a1672f62527e8f83bfca77af4d5cd95b
Bilal1545 <abacar1243@gmail.com>
2026-01-24 17:35:10 +0300

    a

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@ set -e
 
 REPO_URL="https://raw.githubusercontent.com/Bilal1545/bix/main/bix.sh"
 TARGET="/usr/local/bin/bix"
+priv="sudo"
 
 echo "Installing bix to $TARGET"
 
 if [[ $EUID -ne 0 ]]; then
     if command -v doas >/dev/null 2>&1; then
-        exec doas bash "$0"
+        doas true
+        priv="doas"
     elif command -v sudo >/dev/null 2>&1; then
-        exec sudo bash "$0"
-    else
-        echo "Error: need root (sudo or doas)"
-        exit 1
+        sudo -v
     fi
 fi
 
-curl -fsSL "$REPO_URL" -o "$TARGET"
-chmod +x "$TARGET"
+$priv rm -f "$TARGET"
+$priv curl -fsSL "$REPO_URL" -o "$TARGET"
+$priv chmod +x "$TARGET"
 
 echo ""
 echo "bix installed successfully."
